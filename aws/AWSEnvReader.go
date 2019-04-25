4ca897b39bea3e9c863f8d6ee2dd73b542d6c490
Landon Patmore <landon.patmore@gmail.com>
2019-04-24 22:15:47 -0400

    now prints env variables for aws

--- aws/AWSEnvReader.go
+++ aws/AWSEnvReader.go
@@ -1,13 +1,14 @@
 package aws
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
 
 func ReadAWSEnv() (awsRegion string) {
-	_, aFound := os.LookupEnv("AWS_ACCESS_KEY_ID")     // we don't return this because the AWS sdk auto detects this
-	_, sFound := os.LookupEnv("AWS_SECRET_ACCESS_KEY") // we don't return this because the AWS sdk auto detects this
+	accessKey, aFound := os.LookupEnv("AWS_ACCESS_KEY_ID")     // we don't return this because the AWS sdk auto detects this
+	secretKey, sFound := os.LookupEnv("AWS_SECRET_ACCESS_KEY") // we don't return this because the AWS sdk auto detects this
 	rKey, rFound := os.LookupEnv("AWS_REGION")
 
 	if !aFound {
@@ -19,6 +20,10 @@ func ReadAWSEnv() (awsRegion string) {
 	if !rFound {
 		log.Fatal(`The "AWS_REGION" env variable was not set.  Please set it.`)
 	}
+
+	fmt.Println("AWS_ACCESS_KEY_ID: " + accessKey)
+	fmt.Println("AWS_SECRET_ACCESS_KEY: " + secretKey)
+	fmt.Println("AWS_REGION: " + rKey)
 
 	return rKey
 }
